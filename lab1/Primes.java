e89b5377346e311905e7192be17991f80bcd7cb3
Andrey Sopov <saa020403@gmail.com>
2022-09-26 20:47:47 +0300

    isPrime() realization

--- lab1/Primes.java
+++ lab1/Primes.java
@@ -1,5 +1,14 @@
+/**/
 public class Primes {
-    static public void main(String[] args) {
+    public static void main(String[] args) {
 
+    }
+    public static boolean isPrime(int n) {
+        for(int i = 2; i < n; ++i) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
