387fb7faec677aa95ad49e641b62452010dc561c
Andrey Sopov <saa020403@gmail.com>
2022-09-26 23:53:07 +0300

    reverseString() realization

--- lab1/Palindrome.java
+++ lab1/Palindrome.java
@@ -4,4 +4,12 @@ public class Palindrome {
             String s = args[i];
         }
     }
+
+    public static String reverseString(String s) {
+        String res = "";
+        for (int i = s.length() - 1; i >= 0; --i) {
+            res += s.charAt(i);
+        }
+        return res;
+    }
 }
