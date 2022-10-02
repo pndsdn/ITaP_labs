b72c35815bced78a73aa3cf1fc037fbe39f27a00
Andrey Sopov <saa020403@gmail.com>
2022-10-02 23:52:46 +0300

    Point2d -> Point3d

--- lab2/src/Point3d.java
+++ lab2/src/Point3d.java
@@ -1,14 +1,17 @@
-public class Point2d {
+public class Point3d {
     private double xCoord;
     private double yCoord;
 
-    public Point2d() {
-        this(0,0);
+    private double zCoord;
+
+    public Point3d() {
+        this(0,0,0);
     }
 
-    public Point2d(double x, double y) {
+    public Point3d(double x, double y, double z) {
         xCoord = x;
         yCoord = y;
+        zCoord = z;
     }
 
     public double getX() {
@@ -19,11 +22,19 @@ public class Point2d {
         return yCoord;
     }
 
+    public double getZ() {
+        return zCoord;
+    }
+
     public void setX(double x) {
         xCoord = x;
     }
 
     public void setY(double y) {
         yCoord = y;
+    }
+
+    public void setZ(double z) {
+        zCoord = z;
     }
 }
