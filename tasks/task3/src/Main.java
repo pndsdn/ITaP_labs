e9a7fa8ddd16b722fb427d2c61e585cc520f14cf
Andrey Sopov <saa020403@gmail.com>
2022-10-09 12:09:52 +0300

    3_1

--- tasks/task3/src/Main.java
+++ tasks/task3/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    // 3_1
+    public static int solutions(double a, double b, double c) {
+        double D = b*b - 4*a*c;
+        if (D>0) {
+            return 2;
+        }
+        else if (D == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 }
