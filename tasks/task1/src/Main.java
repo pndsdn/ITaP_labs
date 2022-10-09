d8240d4f8fd79e8e2f5879d75eb8fe58b9f793d4
Andrey Sopov <saa020403@gmail.com>
2022-10-09 04:27:32 +0300

    1_10

--- tasks/task1/src/Main.java
+++ tasks/task1/src/Main.java
@@ -56,4 +56,11 @@ public class Main {
         }
         return res;
     }
+
+    public static boolean abcmath(int a, int b, int c) {
+        for (int i = 0; i < b; ++i) {
+            a += a;
+        }
+        return (a % c == 0);
+    }
 }
