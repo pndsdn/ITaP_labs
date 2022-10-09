e6d5d1066178a09cb96cc9e0fe583a56eba8610a
Andrey Sopov <saa020403@gmail.com>
2022-10-09 03:36:57 +0300

    1_1

--- tasks/task1/src/Main.java
+++ tasks/task1/src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int remainder(int num1, int num2) {
+        return (num1 % num2);
+    }
 }
