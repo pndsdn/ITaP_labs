415e7df7fe147b2f5c3ab584089815cfde7729ca
Andrey Sopov <saa020403@gmail.com>
2022-10-09 08:16:22 +0300

    2_8

--- tasks/task2/src/Main.java
+++ tasks/task2/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 
     // 2_1
@@ -79,5 +79,18 @@ public class Main {
             }
         }
         return  true;
+    }
+
+    // 2_8
+    public static boolean isStrangePair(String s1, String s2) {
+        if (s1.length() == 0 || s2.length() == 0) {
+            if (s1.length() == 0 && s2.length() == 0) {
+                return true;
+            }
+            return false;
+        }
+
+        return (s1.charAt(0) == s2.charAt(s2.length()-1) &&
+                s1.charAt(s1.length()-1) == s2.charAt(0));
     }
 }
