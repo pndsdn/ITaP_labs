1919635825cf4588a564ffc784d12dae84058b93
Andrey Sopov <saa020403@gmail.com>
2022-10-09 05:00:54 +0300

    2_1

--- tasks/task2/src/Main.java
+++ tasks/task2/src/Main.java
@@ -2,4 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    // 2_1
+    public static String repeat(String s, int n) {
+        String res = "";
+        for (int i = 0; i < s.length(); ++i) {
+            for (int j = 0; j < n; ++j) {
+                res += s.charAt(i);
+            }
+        }
+        return res;
+    }
 }
