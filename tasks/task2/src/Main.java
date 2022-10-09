d8cffdfe3ed7eabfd7c3d1506cc9f29fd2a00a37
Andrey Sopov <saa020403@gmail.com>
2022-10-09 07:13:59 +0300

    2_6

--- tasks/task2/src/Main.java
+++ tasks/task2/src/Main.java
@@ -56,4 +56,14 @@ public class Main {
         }
         return 0;
     }
+
+    public static int Fibonacci(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return (Fibonacci(n - 2) + Fibonacci(n - 1));
+    }
 }
