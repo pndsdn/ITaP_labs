d7e3114e6b197cafb5d8bbba097203a42ed22f88
Andrey Sopov <saa020403@gmail.com>
2022-10-09 03:42:16 +0300

    1_3

--- tasks/task1/src/Main.java
+++ tasks/task1/src/Main.java
@@ -10,4 +10,8 @@ public class Main {
     public static double triArea(double b, double h) {
         return ((b * h)/2);
     }
+
+    public static int animals(int chickens, int cows, int pigs) {
+        return (chickens*2 + cows*4 + pigs*4);
+    }
 }
