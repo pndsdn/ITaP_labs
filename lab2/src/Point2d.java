9eb20380b4f2f5b7e82b78fbe718c542276366a6
Andrey Sopov <saa020403@gmail.com>
2022-10-18 00:15:34 +0300

    implementation Point2d

--- lab2/src/Point2d.java
+++ lab2/src/Point2d.java
@@ -1,2 +1,39 @@
 public class Point2d {
+    private double xCoord;
+    private double yCoord;
+
+    public Point2d() {
+        this(0,0);
+    }
+
+    public Point2d(double x, double y) {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    public double getX() {
+        return  xCoord;
+    }
+
+    public double getY() {
+        return yCoord;
+    }
+
+    public void setX(double x) {
+        xCoord = x;
+    }
+
+    public void setY(double y) {
+        yCoord = y;
+    }
+
+    public boolean equal(Point2d anotherPoint) {
+        return xCoord == anotherPoint.xCoord &&
+                yCoord == anotherPoint.yCoord;
+    }
+
+    public double distanceTo(Point3d anotherPoint) {
+        return (Math.round(Math.sqrt(Math.pow((anotherPoint.getX() - this.getX()), 2) +
+                Math.pow((anotherPoint.getY() - this.getY()), 2)) * 100) / 100.0);
+    }
 }
