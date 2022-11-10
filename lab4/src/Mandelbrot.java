d2d160d47f36f1b114d9cbe85230138e783b2e2e
Andrey Sopov <saa020403@gmail.com>
2022-11-10 19:50:56 +0300

    Mandelbrot::getInitialRange() implementation

--- lab4/src/Mandelbrot.java
+++ lab4/src/Mandelbrot.java
@@ -1,2 +1,17 @@
-public class Mandelbrot {
+import java.awt.geom.Rectangle2D;
+
+public class Mandelbrot extends FractalGenerator {
+    //set initial range (-2 - 1.5i) - (1 + 1.5i)
+    @Override
+    public void getInitialRange(Rectangle2D.Double range) {
+        range.x = -2;
+        range.y = -1.5;
+        range.height = 3;
+        range.width = 3;
+    }
+
+    @Override
+    public int numIterations(double x, double y) {
+        return 0;
+    }
 }
