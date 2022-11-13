cdd403aab03c396881c55cc6cafe967b414c40f2
Andrey Sopov <saa020403@gmail.com>
2022-11-13 19:16:50 +0300

    Mandelbrot::numIterations() implementation

--- lab4/src/Mandelbrot.java
+++ lab4/src/Mandelbrot.java
@@ -1,6 +1,8 @@
 import java.awt.geom.Rectangle2D;
 
 public class Mandelbrot extends FractalGenerator {
+    public static final int MAX_ITERATIONS = 2000;
+
     //set initial range (-2 - 1.5i) - (1 + 1.5i)
     @Override
     public void getInitialRange(Rectangle2D.Double range) {
@@ -12,6 +14,26 @@ public class Mandelbrot extends FractalGenerator {
 
     @Override
     public int numIterations(double x, double y) {
-        return 0;
+        double currentX = 0;
+        double currentIY = 0;
+        int iCount = 0;
+        while (iCount < MAX_ITERATIONS) {
+            ++iCount;
+            double realPart = currentX * currentX - currentIY * currentIY;
+            double imaginaryPart = 2 * currentX * currentIY;
+            currentX = realPart + x;
+            currentIY = imaginaryPart + y;
+
+            if (currentX * currentX + currentIY * currentIY <= 4) {
+                break;
+            }
+        }
+
+        if (iCount == MAX_ITERATIONS) {
+            return -1;
+        }
+        else {
+            return iCount;
+        }
     }
 }
