26d6fd4166700db34e58a2c2dbeac026eb45d4e5
Andrey Sopov <saa020403@gmail.com>
2022-11-13 20:03:18 +0300

    FractalExplorer::createAndShowGUI() implementation

--- lab4/src/FractalExplorer.java
+++ lab4/src/FractalExplorer.java
@@ -1,3 +1,6 @@
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 public class FractalExplorer {
     private int displaySize;
@@ -11,4 +14,23 @@ public class FractalExplorer {
         complexAreaRange = new Rectangle2D.Double(0, 0, 0, 0);
         fractalGenerator.getInitialRange(complexAreaRange);
     }
+
+    public void creatAndShowGUI() {
+        displayImage = new JImageDisplay(displaySize, displaySize);
+//        displayImage.addMouseListener(new );
+
+        JButton button = new JButton("Reset");
+//        button.addActionListener(new );
+
+        JFrame frame = new JFrame("fractal");
+        frame.setLayout(new BorderLayout());
+        frame.add(displayImage, BorderLayout.CENTER);
+        frame.add(button, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+    
 }
