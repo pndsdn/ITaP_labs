76e1f6bbfa258d0920447bc6f0d4ae632643d4f3
Andrey Sopov <saa020403@gmail.com>
2022-11-13 19:38:10 +0300

    FractalExplorer::FractalExplorer() implementation

--- lab4/src/FractalExplorer.java
+++ lab4/src/FractalExplorer.java
@@ -1,2 +1,14 @@
+import java.awt.geom.Rectangle2D;
 public class FractalExplorer {
+    private int displaySize;
+    private JImageDisplay displayImage;
+    private FractalGenerator fractalGenerator;
+    private Rectangle2D.Double complexAreaRange;
+
+    public FractalExplorer (int size) {
+        displaySize = size;
+        fractalGenerator = new Mandelbrot();
+        complexAreaRange = new Rectangle2D.Double(0, 0, 0, 0);
+        fractalGenerator.getInitialRange(complexAreaRange);
+    }
 }
