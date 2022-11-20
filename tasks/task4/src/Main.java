75568d9a62edc803c4e1cb67ea7ac09978bd68a4
Andrey Sopov <saa020403@gmail.com>
2022-11-20 15:58:19 +0300

    4_1

--- tasks/task4/src/Main.java
+++ tasks/task4/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Bessie(10, 7, "hello my name is Bessie and this is my essay");
+    }
+
+    // 4_1
+    public static void Bessie(int n, int k, String str) {
+        String[] words = str.split(" ");
+        String currWord = words[0];
+        int currLen = currWord.length();
+
+        for (int i = 1; i < n; ++i) {
+            if ((currLen += words[i].length()) <= k) {
+                currWord += " " + words[i];
+            }
+            else {
+                System.out.println(currWord);
+                currWord = words[i];
+                currLen = currWord.length();
+            }
+        }
+
+        if (currLen < k) {
+            System.out.println(currWord);
+        }
+
     }
 }
