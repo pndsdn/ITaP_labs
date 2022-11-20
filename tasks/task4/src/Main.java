d882563f3016a3d3c672f5dcc6fd5711f27d3143
Andrey Sopov <saa020403@gmail.com>
2022-11-20 18:02:04 +0300

    4_3

--- tasks/task4/src/Main.java
+++ tasks/task4/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Bessie(10, 7, "hello my name is Bessie and this is my essay");
+        //Bessie(10, 7, "hello my name is Bessie and this is my essay");
+        System.out.println(toCamelCase("is_modal_open"));
+        System.out.println(toSnakeCase("isModalOpen"));
     }
 
     // 4_1
@@ -23,6 +25,39 @@ public class Main {
         if (currLen < k) {
             System.out.println(currWord);
         }
+    }
 
+    // 4_2
+    public static String[] split(String str) {
+
+        return null;
+    }
+
+    // 4_3
+    public static String toCamelCase(String str) {
+        String res = "";
+        for (int i = 0; i < str.length(); ++i) {
+            if (str.charAt(i) != '_') {
+                res += str.charAt(i);
+            }
+            else {
+                res += (char)((int) str.charAt(i+1) - 32);
+                ++i;
+            }
+        }
+        return res;
+    }
+
+    public static String toSnakeCase(String str) {
+        String res = "";
+        for (int i = 0; i < str.length(); ++i) {
+            if ((int) str.charAt(i) >= 65 && (int) str.charAt(i) <= 90) {
+                res += "_" + (char)((int) str.charAt(i) + 32);
+            }
+            else {
+                res += str.charAt(i);
+            }
+        }
+        return res;
     }
 }
