659f87a1505955949f664ab8c50e2dc35ad0208b
Andrey Sopov <saa020403@gmail.com>
2022-12-13 20:30:18 +0300

    5_8

--- tasks/task5/src/Main.java
+++ tasks/task5/src/Main.java
@@ -1,3 +1,5 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -279,5 +281,25 @@ public class Main {
         }
 
         return res.toString();
+    }
+
+    // 5_8
+    public static String getSha256Hash(String str) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
