a673850fde1ed594c791cb0c53c4d9d5dd5c6e85
Andrey Sopov <saa020403@gmail.com>
2022-12-16 23:44:30 +0300

    URLDepthPair::toString() implementation

--- lab7/src/URLDepthPair.java
+++ lab7/src/URLDepthPair.java
@@ -1,2 +1,14 @@
 public class URLDepthPair {
+    String url;
+    int depth;
+
+    URLDepthPair(String url, int depth) {
+        this.url = url;
+        this.depth = depth;
+    }
+
+    @Override
+    public String toString() {
+        return url + " " + depth;
+    }
 }
