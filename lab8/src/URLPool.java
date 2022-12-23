4576658261829b07061b597df6e4fa7c7df99a08
Andrey Sopov <saa020403@gmail.com>
2022-12-24 01:57:29 +0300

    URLPool class

--- lab8/src/URLPool.java
+++ lab8/src/URLPool.java
@@ -1,2 +1,65 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class URLPool {
+    private final int maxDepth;
+    private int waitingWorkersCount;
+    private final int threadsCount;
+
+    private final Set<String> checkedURLs = new HashSet<>();
+    private final LinkedList<URLDepthPair> linksToCheck = new LinkedList<>();
+    private final LinkedList<URLDepthPair> checkedLinks = new LinkedList<>();
+
+    public URLPool(int maxDepth, int threadsCount) {
+        this.maxDepth = maxDepth;
+        this.threadsCount = threadsCount;
+    }
+
+    public synchronized void addURLPairToCheck(URLDepthPair pair) {
+        if (checkedURLs.add(pair.getUrl())) {
+            if (pair.depth <= maxDepth) {
+                linksToCheck.add(pair);
+                notify();
+            }
+            else {
+                checkedLinks.add(pair);
+            }
+        }
+    }
+
+    public synchronized void addURLPairsToCheck(List<URLDepthPair> pairs) {
+        for (URLDepthPair pair : pairs) {
+            if (checkedURLs.add(pair.getUrl())) {
+                if (pair.depth <= maxDepth) {
+                    linksToCheck.add(pair);
+                }
+                else {
+                    checkedLinks.add(pair);
+                }
+            }
+        }
+        notifyAll();
+    }
+
+    public synchronized void addCheckedURLPairs(List<URLDepthPair> pairs) {
+        checkedLinks.addAll(pairs);
+    }
+
+    public synchronized URLDepthPair getURLPair() {
+        ++waitingWorkersCount;
+        try {
+            while (linksToCheck.isEmpty()) {
+                wait();
+            }
+        }
+        catch (InterruptedException e) {}
+        --waitingWorkersCount;
+        return linksToCheck.removeFirst();
+    }
+
+    public List<URLDepthPair> getSites() {
+        return checkedLinks;
+    }
 }
