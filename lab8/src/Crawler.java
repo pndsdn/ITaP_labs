c0f6b259929872122910a18a06e3aaaf0950c36f
Andrey Sopov <saa020403@gmail.com>
2022-12-24 01:17:09 +0300

    init

---
+++ lab8/src/Crawler.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Crawler {
+    private final LinkedList<URLDepthPair> linksToCheck = new LinkedList<>();
+    private final LinkedList<URLDepthPair> checkedLinks = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+        Crawler crawler = new Crawler();
+        System.out.println("found: " + crawler.scanWebPage(args[0], Integer.parseInt(args[1])));
+    }
+
+    private LinkedList<URLDepthPair> getSites() {
+        return checkedLinks;
+    }
+
+    private LinkedList<URLDepthPair> scanWebPage(String startUrl, int maxDepth) {
+        linksToCheck.add(new URLDepthPair(startUrl, 0));
+        try {
+            process(maxDepth);
+        }
+        catch (NumberFormatException | IOException e) {
+            System.out.println("usage: java Crawler " + startUrl + " " + maxDepth + "\n exception: " + e);
+        }
+        return getSites();
+    }
+
+    private void process(int maxDepth) throws IOException {
+        while (!linksToCheck.isEmpty()) {
+            URLDepthPair currentPair = linksToCheck.removeFirst();
+            if (currentPair.depth <= maxDepth) {
+                processPath(currentPair);
+            }
+        }
+    }
+
+    private void processPath(final URLDepthPair currentPair) throws IOException {
+        if (checkedLinks.contains(currentPair)) {
+            return;
+        }
+
+        SocketClient socketClient = new SocketClient();
+        socketClient.request(currentPair.getPath(), currentPair.getHost(), new SocketClient.SocketCallback() {
+            @Override
+            public void onSuccess(BufferedReader in) {
+                try {
+                    for (String url : HTMLParser.scanHTTPLinks(in)) {
+                        linksToCheck.add(new URLDepthPair(url, currentPair.depth + 1));
+                    }
+                }
+                catch (IOException e) {
+                    currentPair.setScanningException(e);
+                }
+                finally {
+                    checkedLinks.add(currentPair);
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                currentPair.setScanningException(e);
+                checkedLinks.add(currentPair);
+            }
+        });
+    }
+}
