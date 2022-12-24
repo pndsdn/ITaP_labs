7b3ff6867f2f67aed67a659b1618d29745b30192
Andrey Sopov <saa020403@gmail.com>
2022-12-24 03:52:19 +0300

    minor changes

--- lab8/src/Crawler.java
+++ lab8/src/Crawler.java
@@ -49,7 +49,7 @@ public class Crawler {
 
         while (urlPool.isInProgress()) {
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             }
             catch (InterruptedException e) {
                 e.printStackTrace();
